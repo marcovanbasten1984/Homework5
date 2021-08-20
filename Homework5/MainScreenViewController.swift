6edaee80181562e72cada356d26a77f6f7a19694
marcovanbasten1984 <clayton@ukr.net>
2021-08-20 19:20:14 +0300

    the first version

---
+++ Homework5/MainScreenViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainScreenViewController.swift
+//  Homework5
+//
+//  Created by Kostia Prytkov on 8/20/21.
+//  Copyright © 2021 Kostia Prytkov. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+}
+
--- Homework5/ViewController.swift
+++ Homework5/ViewController.swift
@@ -10,16 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  //  @IBOutlet weak var loginButton: UIButton!
+    
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    
+    @IBAction func textFieldEditingChanged(_ sender: UITextField) {
+        
+        loginButton.isEnabled = false
+        // let login = sender.text ?? ""
+        
+        if (loginTextField.text?.contains("@"))! {
+            loginButton.isEnabled = true
+        }
+            
+       
+    
+      func loginButtonTapped() {
+      /*  if ((passwordTextField.text?.isEmpty))! {
+            
+            let alert = UIAlertController(title: "Alert", message: "Enter correct credentials", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        
+        
+        }  else {
+       */
+            let storyboard = UIStoryboard(name: "Main", bundle: .main)
+            let mainScreenViewController = storyboard.instantiateViewController(withIdentifier: "MainScreenViewController")
+            present(mainScreenViewController, animated: true)
+        
+        }
+        
+        
+        
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    }
 
-}
 
