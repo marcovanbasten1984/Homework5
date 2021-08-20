ec96c4b439b5b7ef1b351ba5b6b6e256e16a0ace
marcovanbasten1984 <clayton@ukr.net>
2021-08-20 11:36:42 +0300

    Initial Commit

---
+++ Homework5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Homework5
+//
+//  Created by Kostia Prytkov on 8/20/21.
+//  Copyright © 2021 Kostia Prytkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
